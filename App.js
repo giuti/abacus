401aca54db231a1982c5fa444c29df74ff9b4ae0
Sergio <s.valle.serrano@gmail.com>
2018-10-18 21:25:48 +0200

    Separate matches & table views

--- App.js
+++ App.js
@@ -19,33 +19,39 @@ class HomeScreen extends React.Component {
 class MatchesScreen extends React.Component {
   constructor(props){
     super(props);
-    this.state ={ isLoading: true}
+    this.state = {
+      isMatchesLoading: true
+    }
+  }
+
+  getMatches() {
+    return fetch('http://api.football-data.org/v2/competitions/2014/matches',
+    {
+      headers: {
+        'X-Auth-Token': '2b235ab73c8a4a8b9cbc541da8ab5191'
+      }
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        isMatchesLoading: false,
+        matchesData: responseJson,
+      }, function(){
+
+      });
+
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
   }
 
   componentDidMount(){
-    return fetch('http://api.football-data.org/v2/competitions/2014/matches',
-      {
-        headers: {
-          'X-Auth-Token': '2b235ab73c8a4a8b9cbc541da8ab5191'
-        }
-      })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({
-          isLoading: false,
-          matchesData: responseJson,
-        }, function(){
-
-        });
-
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
+    this.getMatches();
   }
 
   render() {
-    if(this.state.isLoading){
+    if(this.state.isMatchesLoading){
       return(
         <View style={{flex: 1, padding: 20}}>
           <ActivityIndicator/>
@@ -59,8 +65,14 @@ class MatchesScreen extends React.Component {
         <FlatList
           data={this.state.matchesData.matches}
           renderItem={({item}) =>
-            <View>
-              <Text>{item.homeTeam.name} {item.score.fullTime.homeTeam != null ? (item.score.fullTime.homeTeam) : (<Text>?</Text>)} - {item.score.fullTime.awayTeam != null ? (item.score.fullTime.awayTeam) : (<Text>?</Text>)} {item.awayTeam.name}</Text>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Text>
+                {item.homeTeam.name}
+                {item.score.fullTime.homeTeam != null ? (item.score.fullTime.homeTeam) : (<Text>?</Text>)}
+                -
+                {item.score.fullTime.awayTeam != null ? (item.score.fullTime.awayTeam) : (<Text>?</Text>)}
+                {item.awayTeam.name}
+              </Text>
             </View>
           }
           keyExtractor={(item, index) => 'match_'+item.id}
@@ -75,10 +87,52 @@ class MatchesScreen extends React.Component {
 }
 
 class CalculatedTableScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isStandingsLoading: true
+    }
+  }
+
+  getStandings() {
+    return fetch('http://api.football-data.org/v2/competitions/2014/standings',
+    {
+      headers: {
+        'X-Auth-Token': '2b235ab73c8a4a8b9cbc541da8ab5191'
+      }
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        isStandingsLoading: false,
+        standingsData: responseJson,
+      }, function(){
+
+      });
+
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+  }
+
+  componentDidMount(){
+    this.getStandings();
+  }
+
   render() {
+    if(this.state.isStandingsLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Calculated Table Screen</Text>
+        <Text>{this.state.standingsData.standings[0].type}</Text>
       </View>
     );
   }
