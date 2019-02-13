45ea7e61d18dac689a5ccc8d63177ff5903923f9
Sergio <s.valle.serrano@gmail.com>
2019-02-13 21:06:06 +0100

    Sort teams

--- App.js
+++ App.js
@@ -36,8 +36,9 @@ class MatchesScreen extends React.Component {
       var teams = [];
       for (var i = 0; i < responseJson.data.length; i++) {
         teams.push({
+          id: responseJson.data[i].id,
           position: responseJson.data[i].position,
-          id: responseJson.data[i].teamId,
+          teamId: responseJson.data[i].teamId,
           name: responseJson.data[i].name,
           crest: responseJson.data[i].crest,
           played: responseJson.data[i].played,
@@ -164,10 +165,6 @@ class CalculatedTableScreen extends React.Component {
     }
   }
 
-  compare(a, b) {
-    return b.points - a.points// || b.goalsFor - a.goalsFor
-  }
-
   sortTable() {
     var sortedTeams = []
     var groupBy = function(xs, key) {
@@ -177,17 +174,20 @@ class CalculatedTableScreen extends React.Component {
       }, {});
     };
 
-    var teams = this.state.teams//.sort(this.compare)
+    var teams = this.state.teams
     var gaps = this.state.gaps
     var groupedTeams = groupBy(teams, 'points')
 
     for (var key in groupedTeams) {
-      if (groupedTeams[key].length < 2) {
-        sortedTeams.unshift(groupedTeams[key][0]);
-      } else if (groupedTeams[key].length == 2) {
-        var teamA = groupedTeams[key][0];
-        var teamB = groupedTeams[key][1];
-        var diff = gaps[teamA.id]+'_'+gaps[teamB.id] - gaps[teamB.id]+'_'+gaps[teamA.id];
+      var groupTeamsList = groupedTeams[key]
+      var groupedTeamsLength = groupTeamsList.length
+      console.log(groupTeamsList);
+      if (groupedTeamsLength < 2) {
+        sortedTeams.unshift(groupTeamsList[0]);
+      } else if (groupedTeamsLength == 2) {
+        var teamA = groupTeamsList[0];
+        var teamB = groupTeamsList[1];
+        var diff = (gaps[teamA.id+'_'+teamB.id] || 0) - (gaps[teamB.id+'_'+teamA.id] || 0);
         if (diff < 0) {
           sortedTeams.unshift(teamA);
           sortedTeams.unshift(teamB);
@@ -210,11 +210,57 @@ class CalculatedTableScreen extends React.Component {
             } else if (globalFor > 0) {
               sortedTeams.unshift(teamB);
               sortedTeams.unshift(teamA);
+            } else {
+              // Default sorting
+              sortedTeams.unshift(teamA);
+              sortedTeams.unshift(teamB);
             }
           }
         }
-      } else if (groupedTeams[key].length > 2) {
-        
+      } else if (groupedTeamsLength > 2) {
+        var mapTeams = {}
+        for (var v=0; v<groupedTeamsLength; v++) {
+          mapTeams[groupTeamsList[v].id] = {'pts': 0, 'diff': 0};
+        }
+        for (var i=0; i<groupedTeamsLength; i++) {
+          for (var j=0; j<groupedTeamsLength; j++) {
+            if (i != j) {
+              var gap = gaps[groupTeamsList[i].id+'_'+groupTeamsList[j].id];
+              mapTeams[groupTeamsList[i].id].diff += gap || 0
+              mapTeams[groupTeamsList[j].id].diff -= gap || 0
+              console.log(gap);
+              if ( gap< 0) {
+                console.log(groupTeamsList[i].name+' lost against '+groupTeamsList[j].name);
+                mapTeams[groupTeamsList[j].id].pts += 3;
+              } else if (gap > 0) {
+                console.log(groupTeamsList[i].name+' won against '+groupTeamsList[j].name);
+                mapTeams[groupTeamsList[i].id].pts += 3;
+              } else if (gap == 0){
+                console.log(groupTeamsList[i].name+' and '+groupTeamsList[j].name+' draw the match.');
+                mapTeams[groupTeamsList[i].id].pts += 1;
+                mapTeams[groupTeamsList[j].id].pts += 1;
+              } else {
+                console.log(groupTeamsList[i].name+' vs '+groupTeamsList[j].name+' - Not played yet.');
+              }
+            }
+          }
+        }
+        groupedTeams.sort(function(a, b){
+          if (a.group.pts == b.group.pts) {
+            if (a.group.diff == b.group.diff) {
+              if (a.goalsDiff == b.goalsDiff) {
+                b.goalsFor - a.goalsFor
+              } else {
+                b.goalsDiff - a.goalsDiff
+              }
+            } else {
+              return b.group.diff - a.group.diff
+            }
+          } else {
+            return b.group.pts - a.group.pts;
+          }
+        });
+        console.log(groupTeamsList);
       }
     }
 
@@ -234,7 +280,7 @@ class CalculatedTableScreen extends React.Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Calculated Table Screen</Text>
         <FlatList
-          data={this.state.teams}
+          data={this.state.sortedTeams}
           renderItem={({item}) =>
             <View style={{flexDirection: 'row', alignItems: 'center'}}>
               <Text>
